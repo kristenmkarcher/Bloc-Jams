ccd2321372bcb51db3622b0e5863b15f28f0be10
Kristen Karcher <kristenmkarcher@gmail.com>
2017-10-09 21:00:45 -0400

    Assignment 22 events complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,22 +1,22 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-    function revealPoint(points) {
-       for(var i = 0; i < points.length; i++) {
-         points[i].style.opacity = 1;
-         points[i].style.transform = "scaleX(1) translateY(0)";
-         points[i].style.msTransform = "scaleX(1) translateY(0)";
-         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-       }
-     }
-     revealPoint(points);
- };
+
+var revealPoint = function(point) {
+      point.style.opacity = 1;
+      point.style.transform = "scaleX(1) translateY(0)";
+      point.style.msTransform = "scaleX(1) translateY(0)";
+      point.style.WebkitTransform = "scaleX(1) translateY(0)";
+  }
+
+  var animatePoints = function(points) {
+       forEach(points, revealPoint);
+   }
 
 window.onload = function() {
   if (window.innerHeight > 950) {
     animatePoints(pointsArray);
   }
-  
+
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
