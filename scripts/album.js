c1ed5d035febe827fc7103d3a069803cfc6041a2
Kristen Karcher <kristenmkarcher@gmail.com>
2017-10-29 21:48:57 -0400

    Assignment 24 complete

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,22 @@
      ]
  };
 
+ // Third Album
+  var albumMuse = {
+      title: 'Drones',
+      artist: 'Muse',
+      label: 'Warner Bros.',
+      year: '2015',
+      albumArtUrl: 'assets/images/album_covers/drones.jpeg',
+      songs: [
+          { title: 'Dead Inside', duration: '4:22' },
+          { title: 'Psycho', duration: '5:16' },
+          { title: 'Mercy', duration: '3:51' },
+          { title: 'Reapers', duration: '5:59'},
+          { title: 'Drones', duration: '2:49'}
+      ]
+  };
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -42,13 +58,15 @@
      return template;
  };
 
+// #1
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+
  var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
      // #2
      albumTitle.firstChild.nodeValue = album.title;
@@ -67,4 +85,14 @@
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+
+     var albums = [albumPicasso, albumMarconi, albumMuse];
+     var index = 1;
+     albumImage.addEventListener("click", function(event) {
+       setCurrentAlbum(albums[index]);
+       index++;
+       if (index == albums.length) {
+         index = 0;
+       }
+     });
  };
