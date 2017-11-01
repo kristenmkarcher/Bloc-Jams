4ccd9e791a9b5a11c70dac906e34870028237810
Kristen Karcher <kristenmkarcher@gmail.com>
2017-11-01 13:42:12 -0400

    Checkpoint 29 complete

--- scripts/album.js
+++ scripts/album.js
@@ -14,7 +14,7 @@
      ]
  };
 
- // Another Example Album
+ // Second Album
  var albumMarconi = {
      title: 'The Telephone',
      artist: 'Guglielmo Marconi',
@@ -55,31 +55,26 @@
       + '</tr>'
       ;
 
-     return template;
+     return $(template);
  };
 
-// #1
- var albumTitle = document.getElementsByClassName('album-view-title')[0];
- var albumArtist = document.getElementsByClassName('album-view-artist')[0];
- var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
- var albumImage = document.getElementsByClassName('album-cover-art')[0];
- var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-
-
  var setCurrentAlbum = function(album) {
+     var $albumTitle = $('.album-view-title');
+     var $albumArtist = $('.album-view-artist');
+     var $albumReleaseInfo = $('.album-view-release-info');
+     var $albumImage = $('.album-cover-art');
+     var $albumSongList = $('.album-view-song-list');
 
-     // #2
-     albumTitle.firstChild.nodeValue = album.title;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
+     $albumTitle.text(album.title);
+     $albumArtist.text(album.artist);
+     $albumReleaseInfo.text(album.year + ' ' + album.label);
+     $albumImage.attr('src', album.albumArtUrl);
 
-     // #3
-     albumSongList.innerHTML = '';
+     $albumSongList.empty();
 
-     // #4
      for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         $albumSongList.append($newRow);
      }
  };
 
@@ -96,7 +91,6 @@
      setCurrentAlbum(albumPicasso);
 
      songListContainer.addEventListener('mouseover', function(event) {
-       // #1
        if (event.target.parentElement.className === 'album-view-song-item') {
         // event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
         var songItem = getSongItem(event.target);
@@ -110,8 +104,13 @@
   var findParentByClassName = function(element, targetClass) {
     if (element) {
       var currentParent = element.parentElement;
-      while (currentParent.className !== targetClass && currentParent.className !== null) {
-        currentParent = currentParent.parentElement;
+      if (!currentParent) {
+        console.log("No Parent Found");
+      } else {
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
+          console.log("No parent found with that class name");
+          currentParent = currentParent.parentElement;
+        }
       }
       return currentParent;
     }
@@ -155,10 +154,8 @@
 
   for (var i = 0; i < songRows.length; i++) {
       songRows[i].addEventListener('mouseleave', function(event) {
-        // #1
         var songItem = getSongItem(event.target);
         var songItemNumber = songItem.getAttribute('data-song-number');
-        // #2
         if (songItemNumber !== currentlyPlayingSong) {
           songItem.innerHTML = songItemNumber;
         }
